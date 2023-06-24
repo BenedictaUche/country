e5fc11d9541eb192a7f703ce1fee2afd4c286e08
Benedicta Onyebuchi <benedictauche.onyebuchi@gmail.com>
2023-06-24 17:13:39 +0100

    added sortable list by asc aand des

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -1,28 +1,19 @@
 import { useState, useEffect, useMemo } from "react";
 import { Card, Container, Dropdown, DropdownButton } from "react-bootstrap";
+import { Link, useParams } from "react-router-dom";
 import Pagination from "../assets/customhooks/Pagination";
 // import Pagination from "react-js-pagination";
 
-
 export default function Maps() {
-
-  let PageSize = 10
-
-
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   const [selectedRegion, setSelectedRegion] = useState(null);
-  const [currentPage, setCurrentPage] = useState(1);
-
-  const currentTableData = useMemo(() => {
-    const firstPageIndex = (currentPage - 1) * PageSize;
-    const lastPageIndex = firstPageIndex + PageSize;
-    return data.slice(firstPageIndex, lastPageIndex);
-  }, [currentPage]);
-
+  const [sortBy, setSortBy] = useState("name-asc");
+  
+  const history = useParams();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -47,29 +38,43 @@ export default function Maps() {
 
   useEffect(() => {
     if (data) {
-      // Filter data based on search term and selected region
+      // To filter data based on search value and region
       const filteredData = data.filter((country) => {
-        const nameMatch = country.name
-          .toLowerCase()
-          .includes(searchTerm.toLowerCase());
-        const regionMatch =
-          !selectedRegion || country.region === selectedRegion;
+        const nameMatch = country.name.toLowerCase().includes(searchTerm.toLowerCase());
+        const regionMatch = !selectedRegion || selectedRegion === "All Countries" || country.region === selectedRegion;
         return nameMatch && regionMatch;
       });
-      setSearchResults(filteredData);
+
+      //To sort data based on selected option
+      const sortedData = filteredData.sort((x,y) => {
+        const nameOne = x.name.toLowerCase();
+        const nameTwo = y.name.toLowerCase();
+        if(sortBy === 'name-asc') {
+          return nameOne.localeCompare(nameTwo);
+        } else if(sortBy === 'name-desc') {
+          return nameTwo.localeCompare(nameOne);
+        }
+        return 0;
+      })
+      setSearchResults(sortedData);
     }
-  }, [data, searchTerm, selectedRegion]);
+  }, [data, searchTerm, selectedRegion, sortBy]);
+  
 
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
   };
 
   const handleRegionChange = (region) => {
-    setSelectedRegion(region);
+    setSelectedRegion(region === "All Countries" ? null : region);
+  };
+  
+  const handleSortChange = (sortOption) => {
+    setSortBy(sortOption);
   };
 
   if (loading) {
-    return <div>Loading...</div>;
+    return <div className="font-bold text-4xl text-center mt-10">Loading...</div>;
   }
 
   if (error) {
@@ -79,81 +84,107 @@ export default function Maps() {
   return (
     <>
       <Container>
-      <div className="flex justify-between mt-[10%]">
-        <div className="buttons">
-          <input
-            type="text"
-            placeholder="Search for a country"
-            className="form-control bg-gray-800"
-            value={searchTerm}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <DropdownButton
-            id="dropdown-basic-button"
-            className="bg-gray-900 border-none rounded-lg border-gray-900 sm:w-auto max-w-full"
-            title="Filter by Region"
-          >
-            <Dropdown.Item
-              href="#/action-1"
-              className="bg-gray-800 border-none text-white sm:w-auto max-w-full"
-              onClick={() => handleRegionChange("Africa")}
-            >
-              Africa
-            </Dropdown.Item>
-            <Dropdown.Item
-              href="#/action-2"
-              className="bg-gray-800 border-none text-white sm:w-auto max-w-full"
-              onClick={() => handleRegionChange("Americas")}
-            >
-              Americas
-            </Dropdown.Item>
-            <Dropdown.Item
-              href="#/action-3"
-              className="bg-gray-800 border-none text-white sm:w-auto max-w-full"
-              onClick={() => handleRegionChange("Asia")}
-            >
-              Asia
-            </Dropdown.Item>
-            <Dropdown.Item
-              href="#/action-4"
-              className="bg-gray-800 border-none text-white sm:w-auto max-w-full"
-              onClick={() => handleRegionChange("Europe")}
-            >
-              Europe
-            </Dropdown.Item>
-            <Dropdown.Item
-              href="#/action-5"
-              className="bg-gray-800 border-none text-white sm:w-auto max-w-full"
-              onClick={() => handleRegionChange("Oceania")}
-            >
-              Oceania
-            </Dropdown.Item>
-          </DropdownButton>
-        </div>
-      </div>
+        <div className="flex justify-between mt-[10rem] mb-10">
+          <div className="buttons">
+            <input
+              type="text"
+              placeholder="Search for a country"
+              className="form-control bg-white-800"
+              value={searchTerm}
+              onChange={handleChange}
+            />
+          </div>
 
-      <div>
-        {currentTableData.map((country) => (
+          <div>
+            <DropdownButton
+              id="dropdown-sort"
+              className="bg-gray-900 border-none rounded-lg border-gray-900 sm:w-auto max-w-full"
+              title="Sort by"
+            >
+              <Dropdown.Item
+                href="#/name-asc"
+                className="bg-gray-800 border-none text-white sm:w-auto max-w-full"
+                onClick={() => handleSortChange("name-asc")}
+              >
+                Name (Ascending)
+              </Dropdown.Item>
+              <Dropdown.Item
+                href="#/name-desc"
+                className="bg-gray-800 border-none text-white sm:w-auto max-w-full"
+                onClick={() => handleSortChange("name-desc")}
+              >
+                Name (Descending)
+              </Dropdown.Item>
+            </DropdownButton>
+          </div>
+
+
+          <div>
           
-          <Card>
-            <div key={country.name}>
-              <h2>{country.name}</h2>
-              <p>Region: {country.region}</p>
-              <p>Area: {country.area}</p>
-            </div>
-          </Card>
-          
-        ))}
-      </div>
-      <Pagination
-        className="pagination-bar"
-        currentPage={currentPage}
-        totalCount={data.length}
-        pageSize={PageSize}
-        onPageChange={page => setCurrentPage(page)}
-      />
+            <DropdownButton
+              id="dropdown-basic-button"
+              className="bg-gray-900 border-none rounded-lg border-gray-900 sm:w-auto max-w-full"
+              title="Filter by Region"
+            >
+              <Dropdown.Item
+                href="#/All Countries"
+                className="bg-gray-800 border-none text-white sm:w-auto max-w-full"
+                onClick={() => handleRegionChange("All Countries")}
+              >
+                All Countries
+              </Dropdown.Item>
+              <Dropdown.Item
+                href="#/African Countries"
+                className="bg-gray-800 border-none text-white sm:w-auto max-w-full"
+                onClick={() => handleRegionChange("Africa")}
+              >
+                Africa
+              </Dropdown.Item>
+              <Dropdown.Item
+                href="#/Americas"
+                className="bg-gray-800 border-none text-white sm:w-auto max-w-full"
+                onClick={() => handleRegionChange("Americas")}
+              >
+                Americas
+              </Dropdown.Item>
+              <Dropdown.Item
+                href="#/Asian Countries"
+                className="bg-gray-800 border-none text-white sm:w-auto max-w-full"
+                onClick={() => handleRegionChange("Asia")}
+              >
+                Asia
+              </Dropdown.Item>
+              <Dropdown.Item
+                href="#/European Countries"
+                className="bg-gray-800 border-none text-white sm:w-auto max-w-full"
+                onClick={() => handleRegionChange("Europe")}
+              >
+                Europe
+              </Dropdown.Item>
+              <Dropdown.Item
+                href="#/Oceania Countries"
+                className="bg-gray-800 border-none text-white sm:w-auto max-w-full"
+                onClick={() => handleRegionChange("Oceania")}
+              >
+                Oceania
+              </Dropdown.Item>
+            </DropdownButton>
+          </div>
+        </div>
+
+        <div>
+          {searchResults.map((country) => (
+            <Link to={`/country/${country.name}`} onClick={() => history.push(`/country/${country.name}`)}>
+              <Card className="mb-8 p-4">
+                <div key={country.name}>
+                  <h2 className="font-bold text-2xl mb-3">{country.name}</h2>
+                  <p className="font-semibold">Region: {country.region}</p>
+                  <p className="font-semibold">Area: {country.area}</p>
+                </div>
+              </Card>
+            </Link>
+          ))}
+        </div>
       </Container>
     </>
   );
--- src/pages/CountryDetails.jsx
+++ src/pages/CountryDetails.jsx
@@ -1,7 +1,64 @@
-import React from 'react'
+import { Button, Card, Container } from "react-bootstrap";
+import Navbars from "../components/Navbars";
+import { Link, useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export default function CountryDetails() {
+  const { name } = useParams();
+  const [country, setCountry] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchCountryDetails = async () => {
+      try {
+        const response = await fetch(
+          `https://restcountries.com/v2/name/${name}`
+        );
+        if (!response.ok) {
+          throw new Error("Request failed");
+        }
+        const countryData = await response.json();
+        setCountry(countryData[0]);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCountryDetails();
+  }, [name]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
-    <div>CountryDetails</div>
-  )
+    <>
+      <Navbars />
+      <Container>
+        <div className="mt-[7rem]">
+          <Link to="/home">
+            <Button>
+              <i className="fa fa-arrow-left" aria-hidden="true"></i>
+              Back
+            </Button>
+          </Link>
+
+          <div className="mt-10">
+            <Card className="p-4">
+              <h2 className="font-bold text-2xl mb-3">{country.name}</h2>
+              <p className="font-semibold">Region: {country.region}</p>
+              <p className="font-semibold">Area: {country.area}</p>
+            </Card>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
 }
